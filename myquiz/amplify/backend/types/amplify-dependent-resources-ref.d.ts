dd528bbbd5868ed899e91d3b927541a43667982e
SANTHOSH <sansanthosh965@gmail.com>
2024-05-05 13:28:06 +0530

    Initial commit

---
+++ myquiz/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizbe3189e2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
